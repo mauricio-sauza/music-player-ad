e3055d2c154146afc7ae66359618d7b4b391a5d9
Mauricio Sauza <mauriciosauzatorrez666@gmail.com>
2023-08-07 15:12:04 -0400

    Models for the local storage created

---
+++ src/models/ArtistModel.js
@@ -0,0 +1,14 @@
+class ArtistModel {
+    constructor(id, name, genres, members, website, image) {
+      this.id = id;
+      this.name = name;
+      this.genres = genres;
+      this.members = members;
+      this.website = website;
+      this.image = image;
+      // this.albums = []; // You can add albums to this array later
+    }
+  }
+  
+  export default ArtistModel;
+  
